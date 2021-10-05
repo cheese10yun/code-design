b4aa03d575ff0f747e6d728bd316f0611f3a8f26
cheese10yun <cheese10yun@gmail.com>
2021-10-05 23:02:56 +0900

    Add application

---
+++ ch5/src/main/java/com/code/design/Ch5Application.java
@@ -0,0 +1,12 @@
+package com.code.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch5Application.class, args);
+    }
+}
