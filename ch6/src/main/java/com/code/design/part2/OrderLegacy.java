9eec2333f7143ee0820d314eb98b3622f7f22d0a
cheese10yun <cheese10yun@gmail.com>
2021-10-06 03:28:07 +0900

    Add ch6 part2

---
+++ ch6/src/main/java/com/code/design/part2/OrderLegacy.java
@@ -0,0 +1,16 @@
+package com.code.design.part2;
+
+public class OrderLegacy {
+
+    private long id;
+    // KAKAO, SMS, EMAIL 등 메세지 플랫폼등이 있음
+    private String messageTypes;
+
+    public OrderLegacy(String messageTypes) {
+        this.messageTypes = messageTypes;
+    }
+
+    public String getMessageTypes() {
+        return messageTypes;
+    }
+}
---
+++ ch6/src/test/java/com/code/design/part2/OrderLegacyTest.java
@@ -0,0 +1,63 @@
+package com.code.design.part2;
+
+import static org.assertj.core.api.Java6BDDAssertions.then;
+import static org.assertj.core.api.Java6BDDAssertions.thenThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+
+class OrderLegacyTest {
+
+    // Order의 getMessageTypes 메서드를 사용 할 때 불편하다
+    // 안좋은 캡슐화
+    @Test
+    public void anti_message_test_01() {
+        final OrderLegacy orderLegacy = new OrderLegacy("KAKAO,EMAIL,SMS");
+        final String[] split = orderLegacy.getMessageTypes().split(",");
+
+        then(split).contains("KAKAO", "EMAIL", "SMS");
+    }
+
+    @Test
+    public void KAKAO를_KAOKO_라고_잘못_입력했을_경우() {
+        final OrderLegacy orderLegacy = new OrderLegacy("KAOKO,EMAIL,SMS");
+        final String[] split = orderLegacy.getMessageTypes().split(",");
+
+        then(split).doesNotContain("KAKAO");
+        then(split).contains("EMAIL", "SMS");
+    }
+
+    @Test
+    public void 메시지에_KAKAO_EMAIL_SMS_처럼_공백이_들어_간다면_실패한다() {
+        final OrderLegacy orderLegacy = new OrderLegacy("KAKAO, EMAIL, SMS");
+        final String[] split = orderLegacy.getMessageTypes().split(",");
+
+        then(split).contains("KAKAO");
+        then(split).doesNotContain("EMAIL", "SMS");
+    }
+
+    @Test
+    public void 메시지가_없을_때_빈문자열을_보낼_경우() {
+        final OrderLegacy orderLegacy = new OrderLegacy("");
+        final String[] split = orderLegacy.getMessageTypes().split(",");
+
+        then(split).contains("");
+    }
+
+    @Test
+    public void 메시지가_없을_때_null_보내는_경우() {
+        final OrderLegacy orderLegacy = new OrderLegacy(null);
+        thenThrownBy(() -> orderLegacy.getMessageTypes().split(","))
+            .isInstanceOf(NullPointerException.class);
+
+    }
+
+    @Test
+    public void 메시지가_중복으로_올경우() {
+        final OrderLegacy orderLegacy = new OrderLegacy("KAKAO, KAKAO, KAKAO");
+        final String[] split = orderLegacy.getMessageTypes().split(",");
+
+        then(split).contains("KAKAO");
+        then(split).hasSize(3);
+    }
+}
