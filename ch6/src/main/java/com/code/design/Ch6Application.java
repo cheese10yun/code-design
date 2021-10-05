a3cd9adb048b5a86b6e49c4928465e8e2d2d0efa
cheese10yun <cheese10yun@gmail.com>
2021-10-06 02:49:01 +0900

    Add ch6 Init

---
+++ ch6/src/main/java/com/code/design/Ch6Application.java
@@ -0,0 +1,12 @@
+package com.code.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch6Application.class, args);
+    }
+}
