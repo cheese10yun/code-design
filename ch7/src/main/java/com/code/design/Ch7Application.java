6c91d597214656fa694688d0a3c9dec2ca45b6ec
cheese10yun <cheese10yun@gmail.com>
2021-10-07 04:48:45 +0900

    init CH8

---
+++ ch7/src/main/java/com/code/design/Ch7Application.java
@@ -0,0 +1,12 @@
+package com.code.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch7Application.class, args);
+    }
+}
---
+++ ch8/src/main/java/com/code/design/Ch8Application.java
@@ -0,0 +1,12 @@
+package com.code.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch8Application.class, args);
+    }
+}
