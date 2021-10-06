b071af9d7d02c9a9a2816389887341a62adb0940
cheese10yun <cheese10yun@gmail.com>
2021-10-06 23:19:45 +0900

    Add 객채

---
+++ ch2/src/main/java/com/code/design/domain/order/order/dao/OrderFindService.java
@@ -0,0 +1,10 @@
+package com.code.design.domain.order.order.dao;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderFindService {
+
+    // OrderRepository 에대한 로직 구현..
+
+}
